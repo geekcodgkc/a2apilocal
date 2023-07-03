67c66fc8c827f9ad7c21d51fe107712321962841
Carlos Aranguren <arangurencg2@gmail.com>
2023-07-03 19:22:23 -0400

    "add put, post request and handlerError class"

--- send_changes/function.py
+++ send_changes/function.py
@@ -1,18 +1,22 @@
 import json
 import requests
 import pymongo
+import os
 
 
 class send_api_cloud():
 
-    def send_json(url, json):
+    def post(json):
+        URL= "/post"
        # headers = {"Content-Type": "application/json",}
         try:
-             r = requests.post(url, json)
+             r = requests.post(URL, json)
              p = r.json()
              return p
         
         except Exception as error: 
             return str(error)
+        
+   
         
     
