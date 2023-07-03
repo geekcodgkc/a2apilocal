b38d399448b8a9f9413da670e1593104746f348d
Carlos Aranguren <arangurencg2@gmail.com>
2023-07-03 19:20:01 -0400

    "add handlerError class"

--- send_changes/changes.py
+++ send_changes/changes.py
@@ -1,9 +1,11 @@
 import pyodbc
-import requests
 import json
-from function import send_api_cloud
+import sys
+
+#from function import send_api_cloud
 
 class search_changes():
+
     def query_tb_temp():   
         try:
             connect = pyodbc.connect("DSN=A2KSA")
@@ -37,12 +39,13 @@ class search_changes():
                                         INTO PRUEBA_EXI
                                     FROM SINVDEP 
                                     WHERE FT_CODIGODEPOSITO  = 2 
+                                    
                                         """)
                 
             cur_a2.execute("""CREATE INDEX "KEY_CODIGO" ON PRUEBA_EXI (CODIGO_EX) """)
 
             cur_a2.execute("""SELECT 
-                                        CODIGOINV, DESCRIPCION, PRECIOBS, PRECIOBS2, PRECIOUS, PRECIOUS2, EXISTENCIA
+                                    CODIGOINV, DESCRIPCION, PRECIOBS, PRECIOBS2, PRECIOUS, PRECIOUS2, EXISTENCIA
                                     INTO TABLE_B
                                     FROM PRUEBA_INV
                                     INNER JOIN PRUEBA_COSTO  ON CODIGOINV = CODIGO
@@ -58,20 +61,53 @@ class search_changes():
         except Exception as e:
             connect.close()
             return str(e)
-            
-    
-    def deleted_table():
+
+    def convert_tableB_toA():
+        try:
+           connect = pyodbc.connect("DSN=A2KSA")
+           cur = connect.cursor()
+           cur.execute("""SELECT *
+                            INTO TABLE_A
+                            FROM TABLE_B""")
+
+        except Exception as e:
+            print(e)   
+
+    def search_new_register():
         try:
             connect = pyodbc.connect("DSN=A2KSA")
             cur = connect.cursor()
-            cur.execute("""DROP TABLE IF EXISTS TABLE_A """)
-            cur.execute("""DROP TABLE IF EXISTS TABLE_B""")
-            connect.close()
-        except Exception as e:
+            cur.execute("""SELECT CODIGOINV, DESCRIPCION, PRECIOBS, PRECIOUS, PRECIOUS2, EXISTENCIA 
+                            FROM TABLE_B B
+                            LEFT JOIN TABLE_A A ON B.CODIGOINV = A.CODIGOINV
+                            WHERE B.PRECIOUS <> A.PRECIOUS OR B.PRECIOUS2 <> A.PRECIOUS2 
+                            OR B.EXISTENCIA <> A.EXISTENCIA 
+                        """)    
+            new = cur.fetchall()
+            if len(new) > 0:
+                json_new = {}
+                json_new['data'] = []
+                for i in new:
+                    json_new['data'].append({'id_product': i[0],
+                                                'description': i[1],
+                                                'priceUS': i[3],
+                                                'priceUS2': i[4],
+                                                'ext': i[5]
+                                                })
+                print(json_new)    
                 connect.close()
-                return str(e)
+                return json_new
+            else:
+                connect.close()    
+                return '200'    
 
-    def compare_db():
+        except Exception as e:
+            print(e)      
+
+
+
+    def search_update():
+        ###SEARCHING UPDATES IN DATABASE######
         try:
             result = search_changes.query_tb_temp()
             connect = pyodbc.connect("DSN=A2KSA")
@@ -80,9 +116,10 @@ class search_changes():
                             FROM TABLE_B B
                             RIGHT JOIN TABLE_A A ON B.CODIGOINV = A.CODIGOINV
                             WHERE B.PRECIOUS <> A.PRECIOUS OR B.PRECIOUS2 <> A.PRECIOUS2 
-                            OR B.EXISTENCIA <> A.EXISTENCIA 
+                            OR B.EXISTENCIA <> A.EXISTENCIA
                             """)
             table = cursor.fetchall()
+            json_insert = search_changes.search_new_register()
             if len(table) > 0:
                 json = {}
                 json['data']= []
@@ -93,21 +130,25 @@ class search_changes():
                                                 'priceUS2': i[4],
                                                 'ext': i[5]
                                                 })
-                connect.close()
-                try:  
-                    send_api_cloud.send_json()
-                    search_changes.deleted_table()
-                except Exception as error:
-                    print(str(error))         
-                
-                return json 
+                from function import send_api_cloud
+                var_post = send_api_cloud.post(json)
+                if var_post != '400':
+                    search_changes.convert_tableB_toA()
+                else:
+                    sys.exit()    
+
+                connect.close()     
             else: 
                 connect.close()
                 pass
+
+            if json_insert == dict:
+                from function import send_api_cloud
+                send_api_cloud.post(json_insert)
             
         except Exception as e:
             connect.close()
             return str(e)
 
 
-ini = search_changes.compare_db()   
+ini = search_changes.search_update()   
