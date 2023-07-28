2ea1b689992439aeeaa452c520e38b5f3252b4e1
Carlos Aranguren <arangurencg2@gmail.com>
2023-07-28 10:59:17 -0400

    "ADD PATH ECOGRSAS LOCATION AND IMPORT_ENVIROMENT VARIABLES"

--- send_changes/exceptions.py
+++ send_changes/exceptions.py
@@ -3,20 +3,22 @@ import datetime
 
 class Handler_Exceptions():
 
-    def write_fatal_exceptions(error):
-        print(error)
-
+    def write_fatal_exceptions(error, path):
+        time = datetime.datetime.now().strftime('%Y%m%d%H%M')
+        with open("X:/a2appsH/Pagina_Web/ConectorA2/Handler_Exceptions/"+ f"{path}", "a") as file:
+             file.writelines(f"{time}:"+f'[{error}]' +"\n")
+            
     def save_json_to_put_send_client(file:dict, id_client:str):
-        with open(f"//192.168.1.254/a2appsH$/Pagina_Web/ConectorA2/Handler_Errors/Clients_Errors/Put_Errors/{id_client}-PUT.json", "w") as save:
+        with open(f"X:/a2appsH/Pagina_Web/ConectorA2/Handler_Exceptions/Clients_Exceptions/Put_Exceptions/{id_client}.json", "a") as save:
                json.dump(file, save, indent=4)
 
     def save_json_to_put_send_product(file:dict, id_product:str):
-        with open(f"//192.168.1.254/a2appsH$/Pagina_Web/ConectorA2/Handler_Errors/Orders_Errors/Put_Errors/{id_product}-PUT.json", "w") as save:
+        with open(f"X:/a2appsH/Pagina_Web/ConectorA2/Handler_Exceptions/Orders_Errors/Put_Exceptions/{id_product}.json", "a") as save:
                json.dump(file, save, indent=4)
 
     def save_json_to_post_send(file:dict):
          time = datetime.datetime.now().strftime('%Y%m%d%H%M')
-         with open(f"C:/a2CA2020/Empre001/TMP/{time}POST.json",  "w") as save:
+         with open(f"X:/a2appsH/Pagina_Web/ConectorA2/Handler_Exceptions/Products_Exceptions/Post_Exceptions/{time}POST.json",  "a") as save:
               json.dump(file, save, indent=4)    
 
             
