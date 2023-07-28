5ccc3e55485e0e147e74e46edcec66669e69db83
Carlos Aranguren <arangurencg2@gmail.com>
2023-07-28 11:00:10 -0400

    "Add path location ecograsas and fixed error in put products"

--- send_changes/function.py
+++ send_changes/function.py
@@ -3,108 +3,71 @@ import requests
 import os
 from exceptions import Handler_Exceptions
 import sys
-from dotenv import load_dotenv
-
-load_dotenv("C:/ApiRestFlask/PROYECTOS MEJORADOS/CONECTORPE/.env.txt")
-token = os.getenv('API_TOKEN')
-URL = os.getenv('API_URL')
-HEADERS = {'Authorization' : f'Bearer {token}'}
+from import_env import env
 
 class send_api_cloud():
-    global HEADERS
-    global URL    
-        
 
     def post(post):
-        
-                  #   "Content-Type" : "application/json"}
         try:
-             r = requests.post(URL+ '/products', headers=HEADERS , json=post)
+             env_ = env.search_env()
+             r = requests.post(env_[0]+ '/products',headers=env_[1] , json=post)
              p = r.json()
              a= r.elapsed
              print(a)
-             if p == {'error':'hubo un error'}:
-                 Handler_Exceptions.write_fatal_exceptions(p)
+             if p == {"error":"hubo un error", "rawError":{}}:
+                 Handler_Exceptions.save_json_to_post_send(post)
                  return "400"
              return p
         
         except Exception as error:
-            Handler_Exceptions.write_fatal_exceptions(str(error))
-            return "E400"
-            sys.exit() 
+            Handler_Exceptions.save_json_to_post_send(post)
+            return "400"
+         
             
         
     def put(update):
-        URL = "http://200.8.121.68:3001/products"
-        
-        p1 = 'True'
-        p2 = 'True'
-        p3 = 'True'
-        p4 = 'True'
-        
+
+        def prices(x):
+            if x != -1 and x!= 0:
+                return x
+            else:
+                if x == 0:
+                    x= -2
+                    return x
+                return False
+       
         try:
+            env_ = env.search_env()
             for i in update:
                 json_update = {}
-                if i[1] != '0':    
-                    json_update.update({'name': i[1]})
-                else:
-                    pass
-                if i[3] != -1:
-                    json_update.update({'prices':{'p1': i[3]}})
-                else:
-                    p1 = 'False'
-                    pass
-                if i[4] != -1 and p1 == 'True':
-                    p1 = 'True'
-                    json_update.update({'prices':{'p1':i[3], 'p2': i[4]}})
-                else:
-                     if i[4] != -1 and p1 == 'False':
-                        json_update.update({'prices':{'p2':i[4]}})
-                        p2= 'True'
-                     else: 
-                         p2 = 'False'                   
-                if i[5] != -1 and p1 == 'True' and p2 == 'True':
-                    json_update.update({'prices':{'p1':i[3], 'p2': i[4], 'p3': i[5]}})
-                else:
-                     if i[5] != -1 and p2 == 'False' and p1 == 'False':
-                        json_update.update({'prices':{'p3':i[5]}})
-                        p3 = 'True'
-                     elif i[5] != -1 and p1 == 'True':  
-                            json_update.update({'prices': {'p1':i[3], 'p3':i[5]}})
-                            p3 = 'True'
-                     elif i [5] != -1 and p2 == 'True':
-                            json_update.update({'prices': {'p2':i[4], 'p3':i[5]}})
-                            p3 = 'True'  
-                     else:
-                         p3 = 'False'            
-                if i[6] != -1 and json_update['prices'] == {'p1': i[3], 'p2': i[4], 'p3': i[5]}:
-                    json_update.update({'prices':{'p1':i[3], 'p2': i[4], 'p3': i[5], 'p4': i[6]}})
-                    p4 = 'True'
-                else:
-                    if i[6] != -1 and p2 == 'False' and p3 == 'False' and p1 == 'False':
-                        json_update.update({'prices':{'p4':i[6]}})
-                        p4= 'True'
-                    elif i[6] != -1  and p1 == 'True':
-                         json_update.update({'prices':{'p1': i[3], 'p4': i[6]}})
-                         p4= 'True'
-                    elif i[6] != -1 and p2  == 'True':     
-                          json_update.update({'prices':{'p2': i[4], 'p4': i[6]}})
-                          p4= 'True'
-                    elif i[6] != -1 and p3  == 'True':    
-                         json_update.update({'prices':{'p3': i[5], 'p4': i[6]}})
-                         p4= 'True'
-                    else:
-                        p4= False
+                prices_list = filter(prices, i)
+                if (i[3] != -1) or (i[4] != -1) or (i[5] != -1) or (i[6] != -1):
+                    json_update.update({'prices': {}})
+                    for x in prices_list:
+                        if i[3] == x:
+                            json_update['prices']['p1'] = i[3]
+                    
+                        if i[4] == x:
+                            json_update['prices']['p2'] = i[4]
+                    
+                        if i[5] == x :
+                            json_update['prices']['p3'] = i[5]
+                    
+                        if i[6] == x:
+                            json_update['prices']['p4'] = i[6]
+                if i[1] != '0':
+                    json_update['name']= i[1]
                 if i[2] != None:
-                        json_update.update({'status': i[2]})
-                else:
-                     pass                     
+                    json_update['status'] = i[2]                
+                
                 print(json_update) 
                 try:    
                     if len(json_update) > 0:
-                        r = requests.put(URL + f'/{i[0]}', json=json_update)
-                        p = r.json()
-                        print(p)
+                        r = requests.put(env_[0] + f'/products/{i[0]}',headers=env_[1] ,json=json_update)
+                        if r.status_code == 400 and r.text != {'error': 'hubor un error',
+                                                                'rawError': {}}:  
+                            Handler_Exceptions.save_json_to_put_send_product(json_update, id_product=i[0])
+                            return '400'
                     else:
                         pass
                 except Exception as e: 
@@ -113,7 +76,8 @@ class send_api_cloud():
                      return '400'  
         except Exception as error:
                     print(str(error))
-                    Handler_Exceptions.write_fatal_exceptions(str(error))
+                    Handler_Exceptions.save_json_to_put_send_product(json_update, id_product= i[0])
+                    Handler_Exceptions.write_fatal_exceptions(str(error), "Products_Exceptions/Put_Exceptions/Fatal_Exceptions/exceptions_.txt")
                     sys.exit()
 
 
