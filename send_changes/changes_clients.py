3c13424df69916daf026d065f5e916a9fbbafc43
Carlos Aranguren <arangurencg2@gmail.com>
2023-07-28 10:56:25 -0400

    "ADD PATH ECOGRASAS A2LOCATION AND SYSEXIT(0)"

--- send_changes/changes_clients.py
+++ send_changes/changes_clients.py
@@ -3,7 +3,7 @@ import sys
 import pyodbc
 import requests
 from exceptions import Handler_Exceptions
-from import_env import search_env
+from import_env import env
 
 class changes_customer():
 
@@ -14,14 +14,14 @@ class changes_customer():
             connect = pyodbc.connect("DSN=A2KSA")
             cur = connect.cursor()
             cur.execute("""SELECT FC_CODIGO, FC_GKC_INTERNET
-                                INTO "//192.168.1.254/a2appsH$/Pagina_Web/ConectorA2/Clients_/Tmp_clients/GKC_TABLEA"
+                                INTO "X:/a2appsH/Pagina_Web/ConectorA2/Clients_/Tmp_clients/GKC_TABLEA"
                                 FROM SCLIENTES
                             WHERE FC_STATUS = 1 """)
-            cur.execute("""CREATE INDEX IF NOT EXISTS "FC_CODE" ON "//192.168.1.254/a2appsH$/Pagina_Web/ConectorA2/Clients_/Tmp_clients/GKC_TABLEA" ("FC_CODIGO") """)
+            cur.execute("""CREATE INDEX IF NOT EXISTS "FC_CODE" ON "X:/a2appsH/Pagina_Web/ConectorA2/Clients_/Tmp_clients/GKC_TABLEA" ("FC_CODIGO") """)
 
             cur.execute("""SELECT FC_CODIGO, FC_GKC_INTERNET
-                            FROM "//192.168.1.254/a2appsH$/Pagina_Web/ConectorA2/Clients_/Tmp_clients/GKC_TABLEA" A
-                            INNER JOIN "//192.168.1.254/a2appsH$/Pagina_Web/ConectorA2/Clients_/Tmp_clients/GKC_TABLEB" B ON A.FC_CODIGO = B.FC_CODIGO
+                            FROM "X:/a2appsH/Pagina_Web/ConectorA2/Clients_/Tmp_clients/GKC_TABLEA" A
+                            INNER JOIN "X:/a2appsH/Pagina_Web/ConectorA2/Clients_/Tmp_clients/GKC_TABLEB" B ON A.FC_CODIGO = B.FC_CODIGO
                             WHERE A.FC_GKC_INTERNET <> B.FC_GKC_INTERNET """)
             actualiza = cur.fetchall()
             if len(actualiza) > 0:
@@ -30,28 +30,35 @@ class changes_customer():
                 return False
             
         except Exception as e:
-            print(str(e))
-            return 'Error'  
+            Handler_Exceptions.write_fatal_exceptions(str(e), "Clients_Exceptions/Dbisam_Exceptions/errorDbisam.txt")
+            sys.exit(0)
           
     def put_changes():
         try:
+           
             result = changes_customer.compare_GKCtableA_B()
             if type(result) == list:
                 json_update = {}
-                for i in result:
-                    id = i[0]  
-                    json_update.update({'status': i[1]})
-                    env_ = search_env()
-                    r = requests.put(env_[0] + f'/clients/{id}', headers=env_[1], json=json_update)
-                    p = r.request
-                    print(str(p))
-                print(json_update)    
+                try:
+                    for i in result:
+                        id = i[0]  
+                        json_update.update({'verified': i[1]})
+                        env_ = env.search_env()
+                        r = requests.put(env_[0] + f'/clients/{id}', headers=env_[1], json=json_update)
+                        if r.status_code == 200 and r.text != 'null':
+                                print("ACtualizado")
+                                sys.exit
+                        else:    
+                            Handler_Exceptions.save_json_to_put_send_client(json_update, id)   
+                       
+                    print(json_update) 
+                except Exception as e:
+                    Handler_Exceptions.save_json_to_put_send_client(json_update, id['id'])       
             else:
-                print("NADA")
-                sys.exit()        
+                sys.exit(0)       
 
         except Exception as e:
-            Handler_Exceptions.save_json_to_put_send_client(json_update, id)
+            Handler_Exceptions.write_fatal_exceptions(e)
             print(e)    
 
 init = changes_customer.put_changes()            
